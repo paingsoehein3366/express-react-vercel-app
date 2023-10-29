d5c0c29e0fcd0119489e64fadace2921d426866c
PaingSoeHein <pc@pishinoMacBook-Pro.local>
2023-10-29 16:45:42 +0900

    vercel build

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,56 @@
 import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+  const [data, setData] = useState({ email: "", password: "" });
+  const loginFunction = async () => {
+    const response = await fetch("http://localhost:5000/login", {
+      method: "GET",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify(data)
+    });
+
+    console.log("response :", await response.json());
+    console.log("data :", data);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+        <input
+          style={{
+            width: 300,
+            height: 40,
+            margin: 3,
+            padding: "5px 10px"
+          }}
+          placeholder="Email"
+          onChange={(evt) => setData({ ...data, email: evt.target.value })}
+        />
+        <input
+          type="password"
+          style={{
+            width: 300,
+            height: 40,
+            margin: 3,
+            padding: "5px 10px"
+          }}
+          placeholder="Password"
+          onChange={(evt) => setData({ ...data, password: evt.target.value })}
+        />
+        <button
+          style={{
+            width: 100,
+            height: 50,
+            background: "skyblue",
+            borderRadius: 10,
+            border: "none",
+          }}
+          onClick={loginFunction}
+        >Login</button>
+      </div>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,10 +1,24 @@
+const { json } = require("express");
 const express = require("express");
 const app = express();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Server is runing.");
 });
 app.get("/login", (req, res) => {
+    const data = [
+        { name: "paing", email: "paing@email.com", password: "paing" },
+        { name: "soe", email: "soe@email.com", password: "soe" },
+        { name: "heing", email: "heing@email.com", password: "heing" },
+    ];
+    const { email, password } = req.body;
+    const isVaild = email && password;
+    console.log("email:", email, " password:", password);
+    if (!isVaild) return res.send(400).sendStatus("no data");
+    const EmailTag = data.map(item => item.email);
+    console.log("EmailTag", EmailTag);
     res.send("login");
 })
 
